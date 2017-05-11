65ab6a18cdf94e4ffb345d84815e277bc3a60fe0
Gadiel Reyes <gadiel182@gmail.com>
2017-05-11 17:25:37 -0400

    finished refactor of the second video

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -2,29 +2,27 @@ import axios from 'axios'
 
 var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
-var param = "?client_id=" + id + "&client_secret=" + sec;
+var param = `?client_id=${id}&client_secret=${sec}`;
 
 function getUserInfor (username) {
-	return axios.get('https://api.github.com/users/' + username + param);
+	return axios.get(`https://api.github.com/users/${username + param}`);
 }
 
 function getRepos (username) {
-	return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+	return axios.get(`https://api.github.com/users/${username}/repos${param}&per_page=100`);
 }
 
 function getTotalStars (repos) {
-	return repos.data.reduce(function (prev, current) {
-		return prev + current.stargazers_count
-	}, 0)
+	return repos.data.reduce((prev, current) => prev + current.stargazers_count, 0)
 }
 
 function getPlayersData (player) {
 	return getRepos(player.login)
 		.then(getTotalStars)
-		.then(function (totalStars) {
+		.then((totalStars) => {
 			return {
 				followers: player.followers,
-				totalStars: totalStars
+				totalStars
 			}
 		})
 }
@@ -37,13 +35,9 @@ function calculateScores (players) {
 }
 
 export function getPlayersInfo (players) {
-	return axios.all(players.map(function (username) {
-		return getUserInfor(username);
-	})).then(function (info) {
-		return info.map(function (user) {
-			return user.data;
-		})
-	}).catch(function (err) {
+	return axios.all(players.map((username) => getUserInfor(username)))
+		.then((info) => info.map((user) => user.data))
+		.catch(function (err) {
 		console.warn('Error in getPlayersInfo', err);
 	})
 }
@@ -54,7 +48,7 @@ export function battle (players) {
 
 	return axios.all([playerOneData, playerTwoData])
 		.then(calculateScores)
-		.catch(function (err) {
+		.catch((err) => {
 			console.warn('Error in getPlayersInfo: ', err)
 		})
 }
